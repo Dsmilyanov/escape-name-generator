e90d1e2bc0414bfe2299b524c306d312de1e6c95
dsmilyanov <dsmilyanov@gmail.com>
2016-09-09 17:35:58 +0300

    Change name generating algorithm

--- models/name_generator.rb
+++ models/name_generator.rb
@@ -2,8 +2,8 @@ class NameGenerator
   def self.generate(pool)
     words_taken = pool.shuffle.take(rand(2..5))
 
-    if words_taken.size == 2 && rand(1)
-      words_taken.insert(1, 'of')
+    if rand(2).zero?
+      words_taken.insert(words_taken.size - 1, 'of')
     end
 
     return words_taken.join(" ")
