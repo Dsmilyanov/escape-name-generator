b66d6634c672a9539e8b3ab4992b08fa30145a37
dsmilyanov <dsmilyanov@gmail.com>
2016-09-10 14:24:26 +0300

    Add some words

--- start.rb
+++ start.rb
@@ -3,7 +3,7 @@ $: << File.dirname(__FILE__) + "/models"
 require 'sinatra'
 require 'name_generator'
 
-WORDS = ['Escape', 'Room', 'Quiz', 'Level', 'Rooms', 'Question', 'Clue', 'Time', 'Puzzle', 'Puzzles', 'House', 'Secret', 'Game', 'Games', 'Challenge', 'Challenges', 'Logic', 'Mystery', 'Mysteries', 'Exit', 'Key', 'Keys', 'Safe', 'Door', 'Doors']
+WORDS = ['Escape', 'Room', 'Quiz', 'Level', 'Rooms', 'Question', 'Clue', 'Time', 'Puzzle', 'Puzzles', 'House', 'Secret', 'Game', 'Games', 'Challenge', 'Challenges', 'Logic', 'Mystery', 'Mysteries', 'Exit', 'Key', 'Keys', 'Safe', 'Door', 'Doors', 'Solve', 'Mind']
 
 get '/' do
   erb :index
